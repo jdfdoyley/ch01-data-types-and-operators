a4f157c524240dbe0e40543d7e7477e8be033446
Jason D'Oyley <jdfdoyley@gmail.com>
2022-12-19 07:01:21 -0500

    Demo how to use char data type

---
+++ src/CharArithDemo.java
@@ -0,0 +1,22 @@
+/*
+ * Jason D. F. D'Oyley
+ * Dec 19, 2022
+ * Ch02: Introduction to Data Types and Operators
+ * Instruction: Character variables can be handled
+ * like integers.
+ * */
+
+public class CharArithDemo {
+    public static void main(String[] args) {
+        char ch;
+
+        ch = 'X';
+        System.out.println("ch contains " + ch);
+
+        ch++;   // increment ch
+        System.out.println("ch is now " + ch);
+
+        ch = 90;    // give ch the value Z
+        System.out.println("ch is now " + ch);
+    }
+}
